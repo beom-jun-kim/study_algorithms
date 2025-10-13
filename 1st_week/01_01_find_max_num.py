d1c928166e4e5319eae2466a4a519760cdce815c
kimbeomjun <gimb52043@gmail.com>
2025-10-14 00:13:10 +0900

    asdf

--- 1st_week/01_01_find_max_num.py
+++ 1st_week/01_01_find_max_num.py
@@ -1,8 +1,15 @@
 def find_max_num(array):
-    # 이 부분을 채워보세요!
-    return 1
+    # 하나씩 순회하며 모든 수와 비교
+    for number in array:
+        is_max_num = True
+        for compare_number in array:
+            if number < compare_number:
+                is_max_num = False
+        if is_max_num:
+            return number
+    return None
 
 
-print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 8, 2, 4]))
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
 print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
